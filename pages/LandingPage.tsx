813dfc176cf400151270288c582445985988d7a0
unknown <jaume.malgosa32@gmail.com>
2026-02-23 13:36:01 +0100

    navigate on sesion

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from "react"
 import { useNavigate } from 'react-router-dom';
 import { Logo } from '../constants';
