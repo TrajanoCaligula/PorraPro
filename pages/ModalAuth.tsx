68764db48274bb81baa892e8e515dce49bb2553a
unknown <jaume.malgosa32@gmail.com>
2026-02-24 15:41:47 +0100

    Crear porra

--- pages/ModalAuth.tsx
+++ pages/ModalAuth.tsx
@@ -16,7 +16,7 @@ export default function ModalAuth({ isOpen, onClose }: Props) {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-            redirectTo: `${window.location.origin}/dashboard`,
+            redirectTo: `${window.location.origin}`,
       }
     })
     setLoading(false)
